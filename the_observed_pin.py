2ff3de76b623e25bae1438441e4e5ee9e7671d85
Redsnip8 <FCipolone@gmail.com>
2019-06-29 10:55:20 -0400

    Inital try at level 4 kata. Osudeo code added

---
+++ the_observed_pin.py
@@ -0,0 +1,29 @@
+'''
+Can you help us to find all those variations? It would be nice to have a function, that
+returns a list of all variations for an observed PIN with a length of 1 to 8 digits.
+We could name the function get_pins. But please note that all PINs, the observed one and also the results,
+ must be strings, because of potentially leading '0's. We already prepared some test cases for you.
+
+[1][2][3]
+[4][5][6]
+[7][8][9]
+[*][0][>]
+
+'''
+
+def get_pins(observed):
+  '''TODO: This is your job, detective!'''
+
+# takes in a string of numbers that represent a pin. 3452=> 3 4 5 2 not 3,452.
+# The incoming numbers need to used to create all permutations of the string that fit the number pad.
+
+
+
+
+
+  expectations = [('8', ['5','7','8','9','0']),
+                ('11',["11", "22", "44", "12", "21", "14", "41", "24", "42"]),
+                ('369', ["339","366","399","658","636","258","268","669","668","266","369","398","256","296","259","368","638","396","238","356","659","639","666","359","336","299","338","696","269","358","656","698","699","298","236","239"])]
+
+for tup in expectations:
+  test.assert_equals(sorted(get_pins(tup[0])), sorted(tup[1]), 'PIN: ' + tup[0])
