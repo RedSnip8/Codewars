b051f631558b123775bbca4bd4e36e0287deaec8
Redsnip8 <FCipolone@gmail.com>
2019-07-03 20:23:17 -0400

    Added dictionary for permutations matching

--- the_observed_pin.py
+++ the_observed_pin.py
@@ -11,19 +11,19 @@ We could name the function get_pins. But please note that all PINs, the observed
 
 '''
 
+
 def get_pins(observed):
-  '''TODO: This is your job, detective!'''
+    '''TODO: This is your job, detective!'''
+    number_perms = {
+        1: [1, 2, 4], 2: [1, 2, 3, 5], 3: [2, 3, 6], 4: [1, 4, 5, 7], 5: [2, 4, 5, 6, 8], 6: [3, 5, 6, 9],
+        7: [4, 7, 8], 8: [5, 7, 8, 9, 0], 9: [6, 8, 9]
+    }
 
-# takes in a string of numbers that represent a pin. 3452=> 3 4 5 2 not 3,452.
-# The incoming numbers need to used to create all permutations of the string that fit the number pad.
+    print(number_perms)
 
-
-
-
-
-  expectations = [('8', ['5','7','8','9','0']),
+expectations = [('8', ['5','7','8','9','0']),
                 ('11',["11", "22", "44", "12", "21", "14", "41", "24", "42"]),
                 ('369', ["339","366","399","658","636","258","268","669","668","266","369","398","256","296","259","368","638","396","238","356","659","639","666","359","336","299","338","696","269","358","656","698","699","298","236","239"])]
 
 for tup in expectations:
-  test.assert_equals(sorted(get_pins(tup[0])), sorted(tup[1]), 'PIN: ' + tup[0])
+    get_pins(tup[0])
